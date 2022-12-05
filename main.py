b88c861193d5e12369d2fbd4f0c9bd27b53cab85
kclee9575 <lgc1636@naver.com>
2022-12-05 11:08:54 +0900

    base template

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import define
 
 if __name__ == "__main__":
     uvicorn.run(
-        f"app.{define.SERVER_NAME.lower()}:app", host="0.0.0.0", port=9630, reload=True
+        f"app.{define.SERVER_NAME.lower()}:app", host="0.0.0.0", port=9000, reload=True
     )
